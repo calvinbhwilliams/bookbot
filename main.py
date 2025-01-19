0e4cfd5a8d64731614bd0c68188dd81ad573b48b
calvinbhwilliams <calical0@wgu.edu>
2025-01-19 13:50:48 -0700

    added sort and print statements

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_counts = count_characters(text)
+
+    
+    print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
+    for char, count in sort_on(char_counts):
+        print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
 
 
 def get_num_words(text):
@@ -14,5 +21,18 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
+def count_characters(text):
+    chars = {}
+    for char in text.lower():
+        if char.isalpha():
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
+    return chars
+
+def sort_on(char_counts):
+    return sorted(char_counts.items(), key=lambda x: x[1], reverse=True)
 
 main()
